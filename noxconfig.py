1cf61adcd4585b1c5fbfe5f9f0ab7987ab2b918f
Shankar.KV <shankar.vaithiyanadhaswamy@exasol.com>
2025-10-06 18:47:08 +0530

    Fixing anamolies

--- noxconfig.py
+++ noxconfig.py
@@ -1,19 +1,19 @@
 from __future__ import annotations
 
+from collections.abc import Iterable
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Iterable
 
 
 @dataclass(frozen=True)
 class Config:
     root: Path = Path(__file__).parent
     doc: Path = Path(__file__).parent / "doc"
-    source: Path = Path("exasol/script-languages-package-management")
+    source: Path = Path("exasol/script_languages_package_management")
     version_file: Path = (
         Path(__file__).parent
         / "exasol"
-        / "script-languages-package-management"
+        / "script_languages_package_management"
         / "version.py"
     )
     path_filters: Iterable[str] = ()
