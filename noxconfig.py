62c6d77fe687ccefbd2cb37a510af26419ed0e5c
Shankar <shankar.vaithiyanadhaswamy@exasol.com>
2025-10-07 17:06:49 +0530

    #13: Added workflows and dummy tests. Removed: gha-matrix-exasol, py3.9, test coverage threshold (#16)
    fixes #13

--- noxconfig.py
+++ noxconfig.py
@@ -1,22 +1,23 @@
 from __future__ import annotations
 
+from collections.abc import Iterable
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Iterable
 
 
 @dataclass(frozen=True)
 class Config:
     root: Path = Path(__file__).parent
     doc: Path = Path(__file__).parent / "doc"
-    source: Path = Path("exasol/script-languages-package-management")
+    source: Path = Path("exasol/script_languages_package_management")
     version_file: Path = (
         Path(__file__).parent
         / "exasol"
-        / "script-languages-package-management"
+        / "script_languages_package_management"
         / "version.py"
     )
     path_filters: Iterable[str] = ()
+    python_versions = ["3.10", "3.11", "3.12"]
     pyupgrade_args: Iterable[str] = ("--py312-plus",)
     plugins: Iterable[object] = ()
     create_major_version_tags = False
