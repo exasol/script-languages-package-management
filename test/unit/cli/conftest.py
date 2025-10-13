d8d02a28f77a3d2bc7f38078d74e9730e63557ec
Shankar.KV <shankar.vaithiyanadhaswamy@exasol.com>
2025-10-13 18:01:25 +0530

    renaming fixture provided file names

--- test/unit/cli/conftest.py
+++ test/unit/cli/conftest.py
@@ -12,20 +12,20 @@ def some_package_file(tmp_path: Path) -> str:
 
 @pytest.fixture
 def python_binary(tmp_path: Path) -> str:
-    package_file = tmp_path / "python_package"
+    package_file = tmp_path / "python_binary"
     package_file.write_text("python package")
     return str(package_file)
 
 
 @pytest.fixture
 def conda_binary(tmp_path: Path) -> str:
-    package_file = tmp_path / "conda_package"
+    package_file = tmp_path / "conda_binary"
     package_file.write_text("conda package")
     return str(package_file)
 
 
 @pytest.fixture
 def r_binary(tmp_path: Path) -> str:
-    package_file = tmp_path / "r_package"
+    package_file = tmp_path / "r_binary"
     package_file.write_text("r package")
     return str(package_file)
