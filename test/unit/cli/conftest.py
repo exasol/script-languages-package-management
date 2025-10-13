cdb3670f0a907d6f7bea9cd2255243b6204a6140
Shankar.KV <shankar.vaithiyanadhaswamy@exasol.com>
2025-10-13 18:06:06 +0530

    having binary as content

--- test/unit/cli/conftest.py
+++ test/unit/cli/conftest.py
@@ -13,19 +13,19 @@ def some_package_file(tmp_path: Path) -> str:
 @pytest.fixture
 def python_binary(tmp_path: Path) -> str:
     package_file = tmp_path / "python_binary"
-    package_file.write_text("python package")
+    package_file.write_text("<binary>")
     return str(package_file)
 
 
 @pytest.fixture
 def conda_binary(tmp_path: Path) -> str:
     package_file = tmp_path / "conda_binary"
-    package_file.write_text("conda package")
+    package_file.write_text("<binary>")
     return str(package_file)
 
 
 @pytest.fixture
 def r_binary(tmp_path: Path) -> str:
     package_file = tmp_path / "r_binary"
-    package_file.write_text("r package")
+    package_file.write_text("<binary>")
     return str(package_file)
