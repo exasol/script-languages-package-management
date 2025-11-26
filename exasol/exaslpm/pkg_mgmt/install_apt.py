a8b95d75c55f9ebfc5ee366f5ba03f8defc51d8b
Shankar.KV <shankar.vaithiyanadhaswamy@exasol.com>
2025-11-26 21:22:19 +0530

    Adding apt-install

---
+++ exasol/exaslpm/pkg_mgmt/install_apt.py
@@ -0,0 +1,19 @@
+# sgn
+
+import subprocess
+
+from exasol.exaslpm.model.package_file_config import AptPackages
+
+
+def install_via_apt(apt_packages: AptPackages):
+    if apt_packages is not None:
+        for package in apt_packages.packages:
+            command_01 = "apt-get -y update"
+            command_02 = "apt-get -y clean"
+            command_03 = "apt-get -y autoremove"
+            command_04 = f"apt-get install -V -y --dry-run --no-install-recommends {package.name}"
+            command_05 = "key_servers"
+            print(f"Installing - {command_04}")
+            # result = subprocess.run(command, shell=True)
+            # if result.returncode != 0:
+            #    print(f"'{command}' failed with exit code {result.returncode}")
--- exasol/exaslpm/pkg_mgmt/install_packages.py
+++ exasol/exaslpm/pkg_mgmt/install_packages.py
@@ -1,6 +1,29 @@
 import pathlib
 
 import click
+import yaml
+
+from exasol.exaslpm.model.package_file_config import (
+    BuildStep,
+    Package,
+    PackageFile,
+    Phase,
+)
+from exasol.exaslpm.pkg_mgmt.install_apt import install_via_apt
+
+
+def parse_package_file(
+    package_file: PackageFile, phase_name: str, build_step_name: str
+) -> Phase:
+    all_phases = []
+    build_steps = package_file.build_steps
+    for itr_step_name, itr_build_step in build_steps.items():
+        if itr_step_name.lower() == build_step_name.lower():
+            phases = itr_build_step.phases
+            for itr_phase_name, itr_phase in phases.items():
+                if itr_phase_name.lower() == phase_name.lower():
+                    all_phases.append(itr_phase)
+    return all_phases
 
 
 def package_install(
@@ -11,6 +34,7 @@ def package_install(
     conda_binary: pathlib.Path,
     r_binary: pathlib.Path,
 ):
+    """
     click.echo(
         f"Phase: {phase}, \
         Package File: {package_file}, \
@@ -19,6 +43,15 @@ def package_install(
         Conda Binary: {conda_binary}, \
         R Binary: {r_binary}",
     )
+    """
 
     package_content = package_file.read_text()
-    click.echo(package_content)
+    try:
+        yaml_data = yaml.safe_load(package_content)
+        pkg_file = PackageFile.model_validate(yaml_data)
+        all_phases = parse_package_file(pkg_file, phase, build_step)
+        for single_phase in all_phases:
+            if single_phase.apt is not None:
+                install_via_apt(single_phase.apt)
+    except ValueError:
+        print("Error parsing package file")
