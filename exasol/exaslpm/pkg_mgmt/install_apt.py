2bc050dd109b1d857dab85d62a32cbde8799bd5e
Shankar.KV <shankar.vaithiyanadhaswamy@exasol.com>
2025-11-27 16:26:16 +0530

    Incorporating pre-review comments

--- exasol/exaslpm/pkg_mgmt/install_apt.py
+++ exasol/exaslpm/pkg_mgmt/install_apt.py
@@ -1,17 +1,70 @@
 import subprocess
+from typing import List
 
 from exasol.exaslpm.model.package_file_config import AptPackages
 
 
+def prepare_update_command():
+    update_cmd = ["apt-get", "-y", "update"]
+    return update_cmd
+
+
+def prepare_clean_cmd():
+    clean_cmd = ["apt-get", "-y", "clean"]
+    return clean_cmd
+
+
+def prepare_autoremove_cmd():
+    autoremove_cmd = ["apt-get", "-y", "autoremove"]
+    return autoremove_cmd
+
+
+def prepare_ldconfig_cmd():
+    ldconfig_cmd = ["ldconfig"]
+    return ldconfig_cmd
+
+
+def prepare_locale_cmd():
+    locale_cmd = ["locale-gen", "&&", "update-locale", "LANG=en_US.UTF8"]
+    return locale_cmd
+
+
+def prepare_install_cmd(apt_packages: AptPackages):
+    install_cmd = ["apt-get", "install", "-V", "-y", "--no-install-recommends"]
+    if apt_packages.packages is not None:
+        for package in apt_packages.packages:
+            install_cmd.append(f"{package.name}={package.version}")
+    return install_cmd
+
+
+def execute_cmd(command: list[str]):
+    print(f"Executing: {command}")
+    result = subprocess.run(command, capture_output=True)
+    print(
+        "Success"
+        if result.returncode == 0
+        else f"Failed with exit code {result.returncode}"
+    )
+
+
 def install_via_apt(apt_packages: AptPackages):
     if apt_packages is not None:
-        for package in apt_packages.packages:
-            command_01 = "apt-get -y update"
-            command_02 = "apt-get -y clean"
-            command_03 = "apt-get -y autoremove"
-            command_04 = f"apt-get install -V -y --dry-run --no-install-recommends {package.name}"
-            command_05 = "key_servers"
-            print(f"Installing - {command_04}")
-            # result = subprocess.run(command, shell=True)
-            # if result.returncode != 0:
-            #    print(f"'{command}' failed with exit code {result.returncode}")
+        update_cmd = prepare_update_command()
+        execute_cmd(update_cmd)
+
+        install_cmd = prepare_install_cmd(apt_packages)
+        execute_cmd(install_cmd)
+
+        clean_cmd = prepare_clean_cmd()
+        execute_cmd(clean_cmd)
+
+        autoremove_cmd = prepare_autoremove_cmd()
+        execute_cmd(autoremove_cmd)
+
+        locale_cmd = prepare_locale_cmd()
+        execute_cmd(locale_cmd)
+
+        ldconfig_cmd = prepare_ldconfig_cmd()
+        execute_cmd(ldconfig_cmd)
+    else:
+        print("Invalid AptPackagaes")
--- exasol/exaslpm/pkg_mgmt/install_packages.py
+++ exasol/exaslpm/pkg_mgmt/install_packages.py
@@ -1,12 +1,8 @@
 import pathlib
 
-import click
 import yaml
-from typing import List
 
 from exasol.exaslpm.model.package_file_config import (
-    BuildStep,
-    Package,
     PackageFile,
     Phase,
 )
@@ -15,16 +11,14 @@ from exasol.exaslpm.pkg_mgmt.install_apt import install_via_apt
 
 def parse_package_file(
     package_file: PackageFile, phase_name: str, build_step_name: str
-) -> List[Phase]:
-    all_phases = []
+) -> Phase:
     build_steps = package_file.build_steps
-    for itr_step_name, itr_build_step in build_steps.items():
-        if itr_step_name.lower() == build_step_name.lower():
-            phases = itr_build_step.phases
-            for itr_phase_name, itr_phase in phases.items():
-                if itr_phase_name.lower() == phase_name.lower():
-                    all_phases.append(itr_phase)
-    return all_phases
+    build_step = build_steps[build_step_name]
+
+    phases = build_step.phases
+    phase = phases[phase_name]
+
+    return phase
 
 
 def package_install(
@@ -50,9 +44,8 @@ def package_install(
     try:
         yaml_data = yaml.safe_load(package_content)
         pkg_file = PackageFile.model_validate(yaml_data)
-        all_phases = parse_package_file(pkg_file, phase, build_step)
-        for single_phase in all_phases:
-            if single_phase.apt is not None:
-                install_via_apt(single_phase.apt)
+        single_phase = parse_package_file(pkg_file, phase, build_step)
+        if single_phase.apt is not None:
+            install_via_apt(single_phase.apt)
     except ValueError:
         print("Error parsing package file")
