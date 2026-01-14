06c96f9ff018cefa74c57ebcfdf07f0f3aa83854
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2026-01-14 09:46:06 -0300

    Applied formatting again

--- noxconfig.py
+++ noxconfig.py
@@ -10,6 +10,7 @@ class Config(BaseConfig):
     def runners(self):
         return ["ubuntu-24.04", "ubuntu-24.04-arm"]
 
+
 PROJECT_CONFIG = Config(
     root_path=Path(__file__).parent,
     project_name="exaslpm",
