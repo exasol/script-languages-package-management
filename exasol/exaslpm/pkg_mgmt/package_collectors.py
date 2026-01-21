12e46cc1468706b20bd361367d00c7857d42de3b
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2026-01-21 12:54:22 -0300

    Fixed formatting

--- exasol/exaslpm/pkg_mgmt/package_collectors.py
+++ exasol/exaslpm/pkg_mgmt/package_collectors.py
@@ -21,6 +21,7 @@ def collect_conda_packages(build_steps: list[BuildStep]) -> list[CondaPackage]:
     """
     Collects all conda packages as flat list from a list of build steps.
     """
+
     def get_conda_packages(phase: Phase) -> list[CondaPackage]:
         if not phase.conda:
             return []
@@ -33,6 +34,7 @@ def collect_pip_packages(build_steps: list[BuildStep]) -> list[PipPackage]:
     """
     Collects all Pip packages as flat list from a list of build steps.
     """
+
     def get_pip_packages(phase: Phase) -> list[PipPackage]:
         if not phase.pip:
             return []
