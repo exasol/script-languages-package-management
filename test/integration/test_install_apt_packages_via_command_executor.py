bbc92a1152395dab6db83a96b6754c44061938bc
Shankar.KV <shankar.vaithiyanadhaswamy@exasol.com>
2026-03-30 14:43:10 +0530

    Updating test case

--- test/integration/test_install_apt_packages_via_command_executor.py
+++ test/integration/test_install_apt_packages_via_command_executor.py
@@ -1,8 +1,19 @@
+from exasol.exaslpm.pkg_mgmt.context.context import Context
 from test.integration.docker_test_environment.test_logger import StringMatchCounter
 from test.integration.package_utils import ContainsPackages
 
 from exasol.exaslpm.model.serialization import to_yaml_str
 from exasol.exaslpm.pkg_mgmt.install_packages import package_install
+
+
+class CommandExecuteRecorder:
+    def __init__(self, delegate):
+        self._delegate = delegate
+        self.commands: list[list[str]] = []
+
+    def execute(self, cmd_strs: list[str], env=None):
+        self.commands.append(cmd_strs)
+        return self._delegate.execute(cmd_strs, env=env)
 
 
 def test_apt_install(
@@ -33,3 +44,42 @@ def test_apt_install(
 
     # Check that all 'install apt' commands (see install_apt.prepare_all_cmds() for list) succeeded
     assert return_code_counter.result == 7
+
+
+def test_apt_install_updates_before_install(
+    apt_pkg_file_wildcard,
+    local_package_path,
+    docker_executor_context,
+):
+    apt_package_file_yaml = to_yaml_str(apt_pkg_file_wildcard)
+    local_package_path.write_text(apt_package_file_yaml)
+
+    execute_recorder = CommandExecuteRecorder(docker_executor_context.cmd_executor)
+    ctx_with_exe_recorder = Context(
+        cmd_logger=docker_executor_context.cmd_logger,
+        cmd_executor=execute_recorder,
+        history_file_manager=docker_executor_context.history_file_manager,
+        file_access=docker_executor_context.file_access,
+        file_downloader=docker_executor_context.file_downloader,
+        temp_file_provider=docker_executor_context.temp_file_provider,
+    )
+
+    package_install(
+        package_file=local_package_path,
+        build_step_name="build_step_1",
+        context=ctx_with_exe_recorder,
+    )
+
+    update_index = next(
+        index
+        for index, command in enumerate(execute_recorder.commands)
+        if command == ["apt-get", "-y", "update"]
+    )
+    install_index = next(
+        index
+        for index, command in enumerate(execute_recorder.commands)
+        if command[:5]
+        == ["apt-get", "install", "-V", "-y", "--no-install-recommends"]
+    )
+
+    assert update_index < install_index
